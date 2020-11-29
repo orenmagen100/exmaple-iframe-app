7133ea112397ea313da1d6e14f176dbe23cf371d
Oren Magen <orenmagen100@gmail.com>
2020-11-29 18:23:52 +0200

    add shell-sdk as  lib to main shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ClientCommunicationAPI} from 'wm-os-sdk-shell';
+const clientCommunicationAPI = new ClientCommunicationAPI();
+
 
 function App() {
+  const div = document.getElementById('myDiv');
+  clientCommunicationAPI.onSystemChanged((system: any) => {
+    return new Promise( (resolve, reject) => {
+      setTimeout(() => {
+        reject('ok to change system');
+      },  1000);
+    });
+
+  });
+
+  clientCommunicationAPI.onEnvironmentChanged((env: any) => {
+    return new Promise( (resolve, reject) => {
+      setTimeout(() => {
+        resolve('ok to change env');
+      },  1000);
+    });
+
+  });
+
+
+  const save = async () => {
+    try {
+      const saveResponse = await clientCommunicationAPI.save({data: 'saving teach Me Flow'});
+      console.log('(in iframe resolve)', saveResponse);
+    } catch (err) {
+      console.log('(in iframe reject)', err);
+    }
+  }
+
+  const getEnvironments = async () => {
+    try {
+      const envs = await clientCommunicationAPI.getEnvironments();
+      console.log('(in iframe resolve)', envs);
+    } catch (err) {
+      console.log('(in iframe reject)', err);
+    }
+  }
+
+  const getSystems = async () => {
+    try {
+      const sys = await clientCommunicationAPI.getSystems();
+      console.log('(in iframe resolve)', sys);
+    } catch (err) {
+      console.log('(in iframe reject)', err);
+    }
+  }
+
+ const getEnvironmentData = async () => {
+    try {
+      const envData = await clientCommunicationAPI.getEnvironmentData();
+      console.log('(in iframe resolve)', envData);
+    } catch (err) {
+      console.log('(in iframe reject)', err);
+    }
+  }
+
+  const getSystemData = async () => {
+    try {
+      const envData = await clientCommunicationAPI.getEnvironmentData();
+      console.log('(in iframe resolve)', envData);
+    } catch (err) {
+      console.log('(in iframe reject)', err);
+    }
+  }
+
+  const buttonStyle = {
+    width: '200px',
+    height: '30px',
+    margin: '10px'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='parent'>
+      <button onClick={save} style={buttonStyle}> Save </button>
+      <button onClick={getEnvironments} style={buttonStyle}> getEnvironments </button>
+      <button onClick={getSystems} style={buttonStyle}> getSystems </button>
+      <button onClick={getEnvironmentData} style={buttonStyle}> getEnvironmentData </button>
+      <button onClick={getSystemData} style={buttonStyle}> getSystemData </button>
     </div>
   );
 }
